01a239294ea8885c0d3f4f141bf009c21f70c966
laxman <ramlaxman947@gmail.com>
2022-01-28 22:43:56 +0530

    filter component for stays

---
+++ client/src/Components/Home/Filters/StayFilter.js
@@ -0,0 +1,22 @@
+import * as React from "react";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+
+export default function StayFilter() {
+  return (
+    <FormControl>
+      <FormLabel id="demo-row-radio-buttons-group-label">Stay Type</FormLabel>
+      <RadioGroup
+        row
+        aria-labelledby="demo-row-radio-buttons-group-label"
+        name="row-radio-buttons-group"
+      >
+        <FormControlLabel value="PGs" control={<Radio />} label="PGs" />
+        <FormControlLabel value="Houses" control={<Radio />} label="Houses" />
+      </RadioGroup>
+    </FormControl>
+  );
+}
