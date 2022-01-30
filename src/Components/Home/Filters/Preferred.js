b4b01365cb9910eb94c59ecf16a8e6210ec599e4
laxman <ramlaxman947@gmail.com>
2022-01-30 22:32:30 +0530

    Preferred Filter

--- src/Components/Home/Filters/Preferred.js
+++ src/Components/Home/Filters/Preferred.js
@@ -1,32 +1,24 @@
 import * as React from "react";
-import Radio from "@mui/material/Radio";
-import RadioGroup from "@mui/material/RadioGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
 
 export default function PreferredFilter() {
   return (
-    <FormControl>
+    <>
       <FormLabel id="demo-row-radio-buttons-group-label">
         Preferred for
       </FormLabel>
-      <RadioGroup
-        row
-        aria-labelledby="demo-row-radio-buttons-group-label"
-        name="row-radio-buttons-group"
-      >
-        <FormControlLabel
-          value="Professionals"
-          control={<Radio />}
-          label="Professionals"
-        />
-        <FormControlLabel
-          value="Students"
-          control={<Radio />}
-          label="Students"
-        />
-      </RadioGroup>
-    </FormControl>
+      <div className="form-box">
+        <label>
+          <input type="radio" name="preferred" />
+          <div className="circle"></div>
+          <span>Students 👨‍🎓</span>
+        </label>
+        <label>
+          <input type="radio" name="preferred" />
+          <div className="circle"></div>
+          <span>Professionals 👨‍💻</span>
+        </label>
+      </div>
+    </>
   );
 }
