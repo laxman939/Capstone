d5689a724a34a5b2b0d8db5a414dcdd06db0beea
laxman <ramlaxman947@gmail.com>
2022-02-01 01:38:13 +0530

    Provider with store

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles.css";
 
+import { Provider } from "react-redux";
+import store from "../src/Redux/Store";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
