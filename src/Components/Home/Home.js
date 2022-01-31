55c03c9ad6e5693b892ef3aa3efb845ed0b6528c
laxman <ramlaxman947@gmail.com>
2022-02-01 01:42:11 +0530

    Logic for filters and results

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import StayFilter from "./Filters/StayFilter";
 import GenderFilter from "./Filters/GenderFilter";
 import TopLocations from "./Filters/TopLocations";
-import StaysResults from "./StaysResults";
+import PgResults from "./PgResults";
 import "./style.css";
 import RoomFilter from "./Filters/RoomFilter";
 import PreferredFilter from "./Filters/Preferred";
@@ -10,7 +10,14 @@ import SearchLocation from "./Filters/SearchLocation";
 import HouseFilter from "./Filters/HouseFilter";
 import HousePreferredFilter from "./Filters/HousePreferred";
 
+import { useSelector } from "react-redux";
+import HouseResults from "./HouseResults";
+
 export default function Home() {
+  const pgClicked = useSelector((state) => state.Pg.pgClick);
+
+  console.log("pg " + pgClicked);
+
   return (
     <div className="home-section">
       <main className="main-section">
@@ -26,15 +33,34 @@ export default function Home() {
         <hr />
         <div className="filter">
           <StayFilter />
-          <GenderFilter />
-          <PreferredFilter />
-          <RoomFilter />
-          <HouseFilter />
-          <HousePreferredFilter />
+          {!pgClicked ? (
+            <div>
+              <div className="form-box">
+                <HouseFilter />
+              </div>
+              <div className="form-box">
+                <HousePreferredFilter />
+              </div>
+            </div>
+          ) : (
+            <>
+              <div className="form-box">
+                <GenderFilter />
+              </div>
+              <div className="form-box">
+                <PreferredFilter />
+              </div>
+              <div className="form-box">
+                <RoomFilter />
+              </div>
+            </>
+          )}
         </div>
       </aside>
       <section>
-        <StaysResults />
+        <h5 className="result-title"> Results</h5>
+        <br />
+        {pgClicked ? <PgResults /> : <HouseResults />}
       </section>
     </div>
   );
