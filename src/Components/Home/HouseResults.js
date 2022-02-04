262856ab1eda7c62e98ed27330b1c69df177ca80
laxman <ramlaxman947@gmail.com>
2022-02-04 15:18:56 +0530

    Integrated with backend

--- src/Components/Home/HouseResults.js
+++ src/Components/Home/HouseResults.js
@@ -1,57 +1,77 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import Axios from "axios";
 
 function HouseResults() {
+  const [loading, setLoading] = useState(true);
+  const [info, setInfo] = useState([]);
+  // const [timeOutId, setTimeOutId] = useState("");
+
+  //https://need-based-stay.herokuapp.com/filter?q=uppal ---> query
+
+  useEffect(() => {
+    const fetchInfo = async () => {
+      setLoading(true);
+      try {
+        const response = await Axios.get(
+          "https://need-based-stay.herokuapp.com/filter"
+        );
+        setInfo(response.data);
+      } catch (error) {
+        console.error(error.message);
+      }
+      setLoading(false);
+    };
+
+    fetchInfo();
+  }, []);
+
   return (
-    <div className="property-container">
-      <div className="row single-property">
-        <div className="row">
-          <div className="col">
-            <h5>House1</h5>
-            <p>Location</p>
+    <>
+      {loading && <div>Loading please wait....</div>}
+      {!loading &&
+        info.map((house) => (
+          <div className="property-container" key={house._id}>
+            <div className="row single-property">
+              <div className="row">
+                <div className="col">
+                  <h5>{house.propertyName}</h5>
+                  <footer>
+                    <span>Location: </span>
+                    {house.propertyLocation}
+                  </footer>
+                </div>
+                <div className="col">
+                  <p>
+                    <span>Gender: </span>
+                    {house.gender}
+                  </p>
+                </div>
+              </div>
+              <hr />
+              <div className="row">
+                <div className="col">
+                  <img
+                    id="image"
+                    src="https://st4.depositphotos.com/16030310/25210/v/600/depositphotos_252105266-stock-illustration-vector-illustration-silver-golden-letters.jpg"
+                    alt="house"
+                  />
+                </div>
+                <div className="col">
+                  <h5>Details</h5>
+                  <p>
+                    <span></span>
+                    {house.description}
+                  </p>
+                </div>
+                <div className="col">
+                  <h5>Details</h5>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-        <hr />
-        <div className="row">
-          <div className="col">
-            <img
-              id="image"
-              src="https://st4.depositphotos.com/16030310/25210/v/600/depositphotos_252105266-stock-illustration-vector-illustration-silver-golden-letters.jpg"
-              alt="house"
-            />
-          </div>
-          <div className="col">
-            <h5>Details</h5>
-          </div>
-          <div className="col">
-            <h5>Details</h5>
-          </div>
-        </div>
-      </div>
-      <div className="row single-property">
-        <div className="row">
-          <div className="col">
-            <h5>House1</h5>
-            <p>Location</p>
-          </div>
-        </div>
-        <hr />
-        <div className="row">
-          <div className="col">
-            <img
-              id="image"
-              src="https://st4.depositphotos.com/16030310/25210/v/600/depositphotos_252105266-stock-illustration-vector-illustration-silver-golden-letters.jpg"
-              alt="house"
-            />
-          </div>
-          <div className="col">
-            <h5>Details</h5>
-          </div>
-          <div className="col">
-            <h5>Details</h5>
-          </div>
-        </div>
-      </div>
-    </div>
+        ))}
+    </>
   );
 }
 
